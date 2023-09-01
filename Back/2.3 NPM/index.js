12f551bf87d0101930467abfcdfc2ea419f20a77
Javonte Moore <104592164+Von282@users.noreply.github.com>
2023-09-01 17:52:07 -0500

    Add files via upload

---
+++ Back/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require('sillyname');
+
+import superheroes from "superheroes";
+import generateName from "sillyName";
+// var sillyName = generateName();
+superheroes.all;
+
+console.log("I am " + superheroes.random() + "!");
